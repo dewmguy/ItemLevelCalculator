0ad3b360532269bdd4a1364df7f4567631a2671e
BuildTools <unconfigured@null.spigotmc.org>
2024-07-20 04:14:56 -0600

    generating armor and block values with polynomial regression

--- script.js
+++ script.js
@@ -43,36 +43,45 @@ $(document).ready(function() {
   };
   
   const armorBaseCalc = {
-    Back:    ilvl => ilvl * 1.19 + 5.1,
-    Cloth:   ilvl => ilvl * 1.19 + 5.1,
-    Leather: ilvl => ilvl * 2.22 + 10,
-    Mail:    ilvl => ilvl * 4.9 + 29,
-    Plate:   ilvl => ilvl * 9 + 23,
-    Shield:  ilvl => ilvl * 85 / 3 + 133
-  };
-
-  const armorQualityModifiers = {
-    uncommon: 1,
-    rare: {
-      normal: 1.1,
-      Shield: 1.22
-    },
-    epic: {
-      normal: 1.375,
-      Shield: 1.5616
+    artifact: {
+      Cloth: ilvl => -35.9162438072 + 2.7177139937 * ilvl - 0.0069377995 * Math.pow(ilvl, 2) + 0.0000083546 * Math.pow(ilvl, 3),
+      Leather: ilvl => -67.9413659377 + 5.3865646281 * ilvl - 0.0159938709 * Math.pow(ilvl, 2) + 0.0000230294 * Math.pow(ilvl, 3),
+      Mail: ilvl => -134.2869568235 + 11.0241030695 * ilvl - 0.0271961135 * Math.pow(ilvl, 2) + 0.0000320858 * Math.pow(ilvl, 3),
+      Plate: ilvl => 798.1656398430 - 14.0579736482 * ilvl + 0.3181534343 * Math.pow(ilvl, 2) - 0.0015491623 * Math.pow(ilvl, 3) + 0.0000024433 * Math.pow(ilvl, 4),
+      Shield: ilvl => 1875.7150800000 + 7.7787612192 * ilvl + 0.5256944740 * Math.pow(ilvl, 2) - 0.0030091680 * Math.pow(ilvl, 3) + 0.0000050841 * Math.pow(ilvl, 4)
     },
     legendary: {
-      normal: 1.55,
-      Shield: 1.75
+      Cloth: ilvl => -35.9162438072 + 2.7177139937 * ilvl - 0.0069377995 * Math.pow(ilvl, 2) + 0.0000083546 * Math.pow(ilvl, 3),
+      Leather: ilvl => -67.9413659377 + 5.3865646281 * ilvl - 0.0159938709 * Math.pow(ilvl, 2) + 0.0000230294 * Math.pow(ilvl, 3),
+      Mail: ilvl => -134.2869568235 + 11.0241030695 * ilvl - 0.0271961135 * Math.pow(ilvl, 2) + 0.0000320858 * Math.pow(ilvl, 3),
+      Plate: ilvl => 798.1656398430 - 14.0579736482 * ilvl + 0.3181534343 * Math.pow(ilvl, 2) - 0.0015491623 * Math.pow(ilvl, 3) + 0.0000024433 * Math.pow(ilvl, 4),
+      Shield: ilvl => 1211.2180000000 + 7.7787612192 * ilvl + 0.5256944740 * Math.pow(ilvl, 2) - 0.0030091680 * Math.pow(ilvl, 3) + 0.0000050841 * Math.pow(ilvl, 4)
     },
-    artifact: {
-      normal: 1.75,
-      Shield: 1.95
+    epic: {
+      Cloth: ilvl => -35.9162438072 + 2.7177139937 * ilvl - 0.0069377995 * Math.pow(ilvl, 2) + 0.0000083546 * Math.pow(ilvl, 3),
+      Leather: ilvl => -67.9413659377 + 5.3865646281 * ilvl - 0.0159938709 * Math.pow(ilvl, 2) + 0.0000230294 * Math.pow(ilvl, 3),
+      Mail: ilvl => -134.2869568235 + 11.0241030695 * ilvl - 0.0271961135 * Math.pow(ilvl, 2) + 0.0000320858 * Math.pow(ilvl, 3),
+      Plate: ilvl => 798.1656398430 - 14.0579736482 * ilvl + 0.3181534343 * Math.pow(ilvl, 2) - 0.0015491623 * Math.pow(ilvl, 3) + 0.0000024433 * Math.pow(ilvl, 4),
+      Shield: ilvl => 808.8603184150 + 7.7787612192 * ilvl + 0.5256944740 * Math.pow(ilvl, 2) - 0.0030091680 * Math.pow(ilvl, 3) + 0.0000050841 * Math.pow(ilvl, 4)
+    },
+    rare: {
+      Cloth: ilvl => -1.7276726124 + 2.2210828108 * ilvl - 0.0237938117 * Math.pow(ilvl, 2) + 0.0002079170 * Math.pow(ilvl, 3) - 0.0000005395 * Math.pow(ilvl, 4),
+      Leather: ilvl => -4.8585494487 + 5.2507808015 * ilvl - 0.0668166270 * Math.pow(ilvl, 2) + 0.0005340479 * Math.pow(ilvl, 3) - 0.0000013112 * Math.pow(ilvl, 4),
+      Mail: ilvl => 31.4458566610 + 8.0553432081 * ilvl - 0.0852902001 * Math.pow(ilvl, 2) + 0.0007948793 * Math.pow(ilvl, 3) - 0.0000021079 * Math.pow(ilvl, 4),
+      Plate: ilvl => -945.7560305396 + 52.8506875668 * ilvl - 0.6639548312 * Math.pow(ilvl, 2) + 0.0042400859 * Math.pow(ilvl, 3) - 0.0000092706 * Math.pow(ilvl, 4),
+      Shield: ilvl => -1632.9049851859 + 111.4033198457 * ilvl - 1.3258633473 * Math.pow(ilvl, 2) + 0.0091741437 * Math.pow(ilvl, 3) - 0.0000210537 * Math.pow(ilvl, 4)
+    },
+    uncommon: {
+      Cloth: ilvl => 9.3698273840 + 1.1137462410 * ilvl + 0.0002936443 * Math.pow(ilvl, 2),
+      Leather: ilvl => 39.0574346793 + 1.8042785077 * ilvl + 0.0014831968 * Math.pow(ilvl, 2),
+      Mail: ilvl => 74.5806054126 + 3.9339643160 * ilvl + 0.0043662540 * Math.pow(ilvl, 2),
+      Plate: ilvl => -9.1068691810 + 9.7517833041 * ilvl - 0.0037013807 * Math.pow(ilvl, 2),
+      Shield: ilvl => -289.3643455354 + 34.5446475247 * ilvl - 0.0145401229 * Math.pow(ilvl, 2)
     }
   };
-
+  
   const armorSlotCoefficients = {
-    //Shirt:     1/16,
+    Shield:    16/16,
     Chest:     16/16,
     Robe:      16/16,
     Legs:      14/16,
@@ -81,9 +90,8 @@ $(document).ready(function() {
     Feet:      11/16,
     Hands:     10/16,
     Waist:     9/16,
+    Back:      8/16,
     Wrists:    7/16,
-    Back:      5/16,
-    Shield:    1
   };
 
   const weaponTypes = {
@@ -99,7 +107,6 @@ $(document).ready(function() {
     "6": { label: "Polearm", see: 1 },
     "10": { label: "Staff", see: 1 },
     "2": { label: "Bow", see: 0 },
-    "22": { label: "Bow", see: 1 }, //Fake Bow Entry
     "3": { label: "Gun", see: 1 },
     "18": { label: "Crossbow", see: 1 },
     "19": { label: "Wand", see: 1 },
@@ -208,10 +215,10 @@ $(document).ready(function() {
     "36": { label: "Haste Rating", value: 1, type: 1 },
     "37": { label: "Expertise Rating", value: 1, type: 1 },
     "38": { label: "Attack Power", value: (1/2), type: 1 },
-    "43": { label: "Mana Regen MP5", value: (5/2), type: 1 },
+    "43": { label: "Mana Regen MP5", value: (273/200), type: 1 },
     "44": { label: "Armor Penetration Rating", value: (1/7), type: 1 },
     "45": { label: "Spell Power", value: (6/7), type: 1 },
-    "46": { label: "Health Regen HP5", value: (3/2), type: 1 },
+    "46": { label: "Health Regen HP5", value: (91/400), type: 1 },
     "47": { label: "Spell Penetration", value: (4/5), type: 1 },
     "48": { label: "Block Value", value: (2/3), type: 1 },
     "armor": { label: "Bonus Armor", value: (1/14), type: 2 },
@@ -222,7 +229,7 @@ $(document).ready(function() {
     "frost_res": { label: "Resist Frost", value: 1, type: 0 },
     "shadow_res": { label: "Resist Shadow", value: 1, type: 0 },
   };
-  
+
   function populateItemSlots(type) {
     const itemSlotSelect = $('#item-slot');
     let name = type == '4' ? 'Armor' : 'Weapon';
@@ -379,21 +386,11 @@ $(document).ready(function() {
     const slotData = itemSlots[slot];
     if (slotData.armor == 0) { return ''; }
     console.error("generating armor");
-    let qualityModifier;
-    const baseCalc = armorBaseCalc[type];
+    const baseCalc = armorBaseCalc[quality][type];
     const baseValue = baseCalc(level);
     console.log(`baseValue: ${baseValue}`);
-    if (type === 'Shield') {
-        if (quality === 'rare') { qualityModifier = armorQualityModifiers.rare.Shield; }
-        else if (quality === 'epic') { qualityModifier = armorQualityModifiers.epic.Shield; }
-        else if (quality === 'legendary') { qualityModifier = armorQualityModifiers.legendary.Shield; }
-        else if (quality === 'artifact') { qualityModifier = armorQualityModifiers.artifact.Shield; }
-        else { qualityModifier = armorQualityModifiers[quality]; }
-    }
-    else { qualityModifier = armorQualityModifiers[quality].normal || armorQualityModifiers[quality]; }
-    console.log(`qualityModifier: ${qualityModifier}`);
     const slotCoefficient = armorSlotCoefficients[slotData.label];
-    const totalArmor = Math.ceil(baseValue * qualityModifier * slotCoefficient);
+    const totalArmor = Math.ceil(baseValue * slotCoefficient);
     console.log(`totalArmor: ${totalArmor}`);
     return `<div>${totalArmor} Armor</div>`;
   }
@@ -402,13 +399,17 @@ $(document).ready(function() {
     let baseBlock;
     switch (quality) {
       case 'uncommon':
-        baseBlock = 0.4725 * level - 0.00668 * Math.pow(level, 2) + 0.0001745 * Math.pow(level, 3) - 0.00000094 * Math.pow(level, 4) + 0.0000000015 * Math.pow(level, 5);
+        if (level >= 1 && level <= 80) { baseBlock = -3.0432462933 + 0.8825715740 * level - 0.0312968732 * Math.pow(level, 2) + 0.0007254284 * Math.pow(level, 3) - 0.0000046519 * Math.pow(level, 4); }
+        else if (level >= 81 && level <= 137) { baseBlock = -16.3908655872 + 0.6897096083 * level + 0.0013506088 * Math.pow(level, 2); }
+        else if (level >= 138) { baseBlock = 958.2984725669 - 18.2166307096 * level + 0.1250629715 * Math.pow(level, 2) - 0.0002655468 * Math.pow(level, 3); }
         break;
       case 'rare':
-        baseBlock = 3.0 + 0.12125 * level + 0.00838 * Math.pow(level, 2) - 0.000018 * Math.pow(level, 3) + 0.00000001 * Math.pow(level, 4) - 0.0000000001 * Math.pow(level, 5);
+        if (level >= 1 && level <= 70) { baseBlock = -3.5159255097 + 1.1796186496 * level - 0.0464979829 * Math.pow(level, 2) + 0.0011133817 * Math.pow(level, 3) - 0.0000079076 * Math.pow(level, 4); }
+        else if (level >= 71 && level <= 153) { baseBlock = -29.0000000000 + 1.0000000000 * level; }
+        else if (level >= 154) { baseBlock = 18470.4850615834 - 421.5128438990 * level + 3.5991532696 * Math.pow(level, 2) - 0.0135272648 * Math.pow(level, 3) + 0.0000189300 * Math.pow(level, 4); }
         break;
       default: // epic or better
-        baseBlock = 3.0 + 0.35622 * level + 0.0057 * Math.pow(level, 2) + 0.0000505 * Math.pow(level, 3) - 0.000000507 * Math.pow(level, 4) + 0.000000001 * Math.pow(level, 5);
+        baseBlock = 54.5284833805 - 1.7511894287 * level + 0.0378136180 * Math.pow(level, 2) - 0.0001827142 * Math.pow(level, 3) + 0.0000002842 * Math.pow(level, 4);
         break;
     }
     const totalBlock = Math.ceil(baseBlock);
@@ -440,10 +441,10 @@ $(document).ready(function() {
       case "36": return `Improves haste rating by ${statAmount}.`;
       case "37": return `Improves expertise rating by ${statAmount}.`;
       case "38": return `Increases attack power by ${statAmount}.`;
-      case "43": return `Restores ${statAmount} mana per 5 sec.`;
+      case "43": return `Restores +${statAmount} mana per 5 sec.`;
       case "44": return `Increases your armor penetration rating by ${statAmount}.`;
       case "45": return `Increases spell power by ${statAmount}.`;
-      case "46": return `Restores ${statAmount} health per 5 sec.`;
+      case "46": return `Restores +${statAmount} health per 5 sec.`;
       case "47": return `Increases spell penetration by ${statAmount}.`;
       case "48": return `Increases the block value of your shield by ${statAmount}.`;
       default: return `broken`;
@@ -690,7 +691,7 @@ $(document).ready(function() {
       });
       console.log(`itemValue: ${itemValue}`);
       let i = 0;
-      while (i < 999) {
+      while (i < 9999) {
         const statBudgetIncremental = Math.ceil(Math.pow(itemQualityCoefficient(i) * itemSlot, 1.5));
         if (statBudgetIncremental >= Math.ceil(itemValue)) {
           itemLevel = i;
